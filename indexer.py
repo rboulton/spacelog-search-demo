3012ce4c2e73a0dbf5adfdecf1930cce7ab562ed
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2015-09-25 18:42:43 +0100

    Add a stemmed variant of the `text` field

--- indexer.py
+++ indexer.py
@@ -12,7 +12,46 @@ class Index(object):
     def create(self):
         if self.es.indices.exists(index=self.index_name):
             self.es.indices.delete(index=self.index_name)
-        self.es.indices.create(index=self.index_name)
+        self.es.indices.create(index=self.index_name, body={
+            "settings": {
+                "index": {
+                    "analysis": {
+                        "analyzer": {
+                            "with_stemming": {
+                                "type" : "custom",
+                                "filter" : [
+                                    "standard",
+                                    "lowercase",
+                                    "stemmer_english",
+                                ],
+                                "tokenizer" : "standard"
+                            },
+                        },
+                        "filter" : {
+                            "stemmer_english" : {
+                                "type" : "stemmer",
+                                "name" : "porter2"
+                            },
+                        }
+                    }
+                }
+            },
+            "mappings": {
+                "logline": {
+                    "properties": {
+                        "text": {
+                            "type": "string",
+                            "fields": {
+                                "stemmed": {
+                                    "type": "string",
+                                    "analyzer": "with_stemming",
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        })
 
     def add(self, line):
         id = line.pop("_id")
